c015e626b5b42c411ae9cab0d4234d373ea25954
flori <florian.fourquenay@outlook.com>
2024-04-09 19:13:06 +0200

    V1.6.0 - Correction

--- src/main/java/fr/florianpal/fauction/managers/commandManagers/BidCommandManager.java
+++ src/main/java/fr/florianpal/fauction/managers/commandManagers/BidCommandManager.java
@@ -1,19 +1,4 @@
 
-/*
- * Copyright (C) 2022 Florianpal
- *
- * This program is free software;
- * you can redistribute it and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, see <http://www.gnu.org/licenses/>.
- *
- * Last modification : 07/01/2022 23:07
- *
- *  @author Florianpal.
- */
-
 package fr.florianpal.fauction.managers.commandManagers;
 
 import fr.florianpal.fauction.FAuction;
@@ -26,7 +11,6 @@ import org.bukkit.inventory.ItemStack;
 import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
-
 
 public class BidCommandManager {
     private final BidQueries bidQueries;
--- src/main/java/fr/florianpal/fauction/queries/BidQueries.java
+++ src/main/java/fr/florianpal/fauction/queries/BidQueries.java
@@ -1,19 +1,4 @@
 
-/*
- * Copyright (C) 2022 Florianpal
- *
- * This program is free software;
- * you can redistribute it and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, see <http://www.gnu.org/licenses/>.
- *
- * Last modification : 07/01/2022 23:07
- *
- *  @author Florianpal.
- */
-
 package fr.florianpal.fauction.queries;
 
 import fr.florianpal.fauction.FAuction;
